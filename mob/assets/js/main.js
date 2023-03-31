83c166cf04ca22b282efa2ccca5a58c72f0dc634
desenvolvimento15 <desenvolvimento15@dl.com.br>
2023-03-31 11:53:17 -0300

    vs01

---
+++ mob/assets/js/main.js
@@ -0,0 +1,14 @@
+
+$('.slick').slick({
+    arrows: false,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
+
+
